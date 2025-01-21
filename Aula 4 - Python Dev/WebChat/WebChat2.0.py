5c8b333bd67de1494ee348c08098be9001e5ca61
Wagner Stefano <wagnerstefano98@hotmail.com>
2025-01-21 19:02:50 -0300

    WebChat

---
+++ Aula 4 - Python Dev/WebChat/WebChat2.0.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template #estrutura para criar o site
+from flask_socketio import SocketIO, send #estrutura para criar o chat
+
+app = Flask(__name__)#cria o site
+app.config["SECRET"]= "chaveDeSegurançaAleatoria" #chave de segurança
+app.config["DEBUG"] = True #Teste de código (será removido)
+SocketIO = SocketIO(app, cors_allowed_origins="*") #cria conexão entre diferentes maquinas
+
+@SocketIO.on("message") #define que a função abaixo vai ser acionada quando o evento "message" for ativado
+def gerenciar_mensagens(mensagem):
+    print(f"mensagem:  {mensagem}")
+    send(mensagem, broadcast = True) # envia a mensagem para todos no site
+
+@app.route("/") # inicia a rota do site
+def home():
+    return render_template("index.html") # o app deve carregar o arquivo HTML
+
+if __name__ == "__main__":
+SocketIO.run(app, host = 'localhost') # define que o app vai iniciar em servidor local usando a minha rede
--- Aula 4 - Python Dev/webchat.py
+++ Aula 4 - Python Dev/webchat.py
@@ -28,28 +28,58 @@ import flet as ft
 def main (pagina): 
     #titulo
     titulo = ft.Text("WebChat")
-    pagina.add(titulo)
+
+    def enviar_mensagem(evento):
+        texto = campo_enviar_mensagem.value
+        chat.controls.append(ft.Text(texto))
+        pagina.update
+
+    campo_enviar_mensagem = ft.TextField(label=("Digite sua mensagem"))
+    botao_enviar = ft.ElevatedButton("Enviar", on_click=enviar_mensagem)
+
+    chat = ft.Column()
+    linha_enviar = ft.Row(campo_enviar_mensagem, botao_enviar)
+
+
+
+    def entrar_no_chat(evento):
+        popup.open = False
+
+        pagina.remove(titulo)
+
+        pagina.remove(botao)
+
+        pagina.add(chat)
+
+        pagina.add(campo_enviar_mensagem)
+
+        pagina.add(botao_enviar )
+
+        pagina.update()
+
 
     #criar o popup
-    titulo_popup= ft.Text(title=("Bem vindo ao WebCH=hat"))
-    caixa_nome = ft.TextField()
-    botao_popup = ft.ElevatedButton("Entrar no Chat")
+    titulo_popup= ft.Text("Bem vindo ao WebChat")
+    caixa_nome = ft.TextField(label="Digite o seu nome")
+    botao_popup = ft.ElevatedButton("Entrar no Chat", on_click=entrar_no_chat)
     
 
     popup = ft.AlertDialog(title=titulo_popup, content=caixa_nome, 
-                            actions= [botao_popup  ])
+                            actions=[botao_popup])
 
 
 
     #botão inicial
     def abrir_popup(evento):
-        pagina.dialog=popup
+        pagina.dialog = popup
+        popup.open = True
+        pagina.update()
 
     botao = ft.ElevatedButton("Iniciar Chat", on_click=abrir_popup)
 
     #colocar os elementos na pagina
     pagina.add(titulo)
-    pagina.add(botao)
+    pagina.add(botao_popup)
 
 #executar a função do App com o Flet
 ft.app(main, view=ft.WEB_BROWSER)
